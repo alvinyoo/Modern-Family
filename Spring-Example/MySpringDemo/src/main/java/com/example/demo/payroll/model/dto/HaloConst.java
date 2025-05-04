8cc795cddce55f33f06e7b497beb35c24176c7e0
alvinyoo <63205507+alvinyoo@users.noreply.github.com>
2025-05-03 22:35:41 -0500

    Delete Spring-Example/MySpringDemo directory

--- Spring-Example/MySpringDemo/src/main/java/com/example/demo/payroll/model/dto/HaloConst.java
+++
@@ -1,6 +0,0 @@
-package payroll.model.dto;
-
-public class HaloConst {
-
-    public static String  USER_SESSION_KEY="user_session";
-}
--- Spring-Example/MySpringDemo/src/main/java/com/example/demo/payroll/utils/Java8Feature.java
+++
@@ -1,7 +0,0 @@
-package payroll.utils;
-
-/**
- * Java8 主要有以下几个特性
- */
-public class Java8Feature {
-}
